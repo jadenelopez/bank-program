9ff9b3153fece406ccfc2995c7b0836d1fd48374
jadenelopez <jadenelopez@gmail.com>
2022-05-19 22:20:06 -0700

    Start of Project

---
+++ src/BankMain.java
@@ -0,0 +1,13 @@
+/***
+ * Programmer: Jaden Lopez
+ * Date: 05/19/22
+ * Class Purpose: Main class for my bank program. Everything will run here
+ *
+ */
+public class BankMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hi");
+    }
+}
